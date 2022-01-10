26436a476ada92b5a197896db2dafd915fd92636
hkukuruzov1 <hkukuruzov1@etf.unsa.ba>
2022-01-10 19:30:30 +0100

    Spirala uradjena prije orginalnog roka

--- public/js/test.js
+++ public/js/test.js
@@ -25,6 +25,31 @@ describe('GET', function() {
 
         
 });
+describe('POST', function() {
+    beforeEach(function() {
+      this.xhr = sinon.useFakeXMLHttpRequest();
+   
+      this.requests = [];
+      this.xhr.onCreate = function(xhr) {
+        this.requests.push(xhr);
+      }.bind(this);
+    });
+   
+    afterEach(function() {
+      this.xhr.restore();
+    });
+   
+    it('Test ispravnih podataka', function() {
+      let jsonica = {"brojVjezbi": 2, "brojZadataka": [1,1]};
+      let dataJson = JSON.stringify(jsonica);
+              VjezbeAjax.posaljiPodatke(jsonica,function(err, data) {
+                  assert.equal(err, null, 'Podaci su ispravni!');
+                  assert.deepEqual(data, { "brojVjezbi": 2, "brojZadataka": [1, 1] });
+                  done();
+              });
+              this.requests[0].requestBody.should.equal(dataJson);
+          });
+  });
 describe('Provjera id input polja', function() {
     beforeEach(function() {
       this.xhr = sinon.useFakeXMLHttpRequest();
@@ -65,6 +90,48 @@ describe('Provjera id input polja', function() {
               }
            
         });
+        
+  });
+  describe('Provjera vjezbe.html', function() {
+    beforeEach(function() {
+      this.xhr = sinon.useFakeXMLHttpRequest();
+   
+      this.requests = [];
+      this.xhr.onCreate = function(xhr) {
+        this.requests.push(xhr);
+      }.bind(this);
+    });
+   
+    afterEach(function() {
+      this.xhr.restore();
+
+    });
+   
+    it('Provjera koji id imaju dodana input polja', function() {
+              var pomocniDiv = document.getElementById('vizbe');
+              VjezbeAjax.iscrtajVjezbe(pomocniDiv, { "brojVjezbi": 5, "brojZadataka": [4,3,2,1,10] });
+                let br=1;
+              for (let i = 0; i < pomocniDiv.children.length; i += 2) {
+                  let id = 'v' + br;
+                  assert.equal(pomocniDiv.children[i].id === id, true, 'Id nisu ok, provjerite djecu!');
+              br++;
+              
+                }
+             
+          });
+          it('Provjera koji id imaju dodana input polja2', function() {
+            var pomocniDiv = document.getElementById('vizbe1');
+            VjezbeAjax.iscrtajVjezbe(pomocniDiv, { "brojVjezbi": 5, "brojZadataka": [4,3,2,1,10] });
+              let br=1;
+            for (let i = 0; i < pomocniDiv.children.length; i += 2) {
+                let id = 'v' + br;
+                assert.equal(pomocniDiv.children[i].id === id, true, 'Id nisu ok, provjerite djecu!');
+            br++;
+            
+              }
+           
+        });
+    
   });
   
   
